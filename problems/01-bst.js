0a12594c812bb94163572598069770e673f00f94
Tran <tranle97@gmail.com>
2021-03-01 18:47:17 -0600

    bst

--- problems/01-bst.js
+++ problems/01-bst.js
@@ -1,24 +1,44 @@
 class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class BST {
     // Initialize an empty binary search tree
     constructor() {
-        // Your code here
+        this.root = null;
     }
 
     // Insert a node into the binary search tree
-    insert(val, currentNode=this.root) {
-        // Your code here
+    insert(val, currentNode = this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val);
+        } else {
+            this.insertHelper(val, this.root);
+        }
+    }
+
+    insertHelper(val, node) {
+        if (val < node.val) {
+            if (!node.left) {
+                node.left = new TreeNode(val);
+            } else {
+                this.insertHelper(val, node.left);
+            }
+        } else {
+            if (!node.right) {
+                node.right = new TreeNode(val);
+            } else {
+                this.insertHelper(val, node.right);
+            }
+        }
     }
 
     // Perform a recursive search through the binary search tree
-    searchRecur(val, currentNode=this.root) {
+    searchRecur(val, currentNode = this.root) {
         // Your code here
     }
 
